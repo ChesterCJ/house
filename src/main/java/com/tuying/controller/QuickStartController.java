460fbc1db3fc74d5d3a50efd292072ac3c5df35d
chenjun <chenjun@njdyk.cn>
2021-06-18 15:22:02 +0800

    init house

---
+++ src/main/java/com/tuying/controller/QuickStartController.java
@@ -0,0 +1,21 @@
+package com.tuying.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author chenjun
+ * @version 1.0
+ * @date 2021/6/18
+ */
+@Controller
+public class QuickStartController {
+
+    @ResponseBody
+    @RequestMapping("/index")
+    public String quick() {
+        return "chester 个人博客!";
+    }
+
+}

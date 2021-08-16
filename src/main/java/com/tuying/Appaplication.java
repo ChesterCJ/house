28b62adb32bb7298ac231648f3f0db90588072b1
chenjun <chenjun@njdyk.cn>
2021-08-16 22:27:35 +0800

    init house

--- src/main/java/com/tuying/Appaplication.java
+++ src/main/java/com/tuying/Appaplication.java
@@ -3,6 +3,7 @@ package com.tuying;
 import com.tuying.config.DruidDBConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0
  * @date 2021/6/21
  */
-@EnableAutoConfiguration
-@RestController
+@SpringBootApplication
 @Import(DruidDBConfig.class)
 public class Appaplication {
 
